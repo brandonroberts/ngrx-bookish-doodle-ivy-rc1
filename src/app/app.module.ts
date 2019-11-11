01c2331c12fb917637490caeeb34453d85bcb1f5
Brandon Roberts <brandon@nrwl.io>
2019-11-11 13:09:46 -0600

    add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { EffectsModule } from '@ngrx/effects';
+import { CounterEffects } from './state/counter.effects';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import * as fromCounter from './state/counter.reducer';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ }, {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    StoreModule.forFeature(fromCounter.counterFeatureKey, fromCounter.reducer),
+    EffectsModule.forRoot([]),
+    EffectsModule.forFeature([CounterEffects]),
+    StoreDevtoolsModule.instrument()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/state/counter.effects.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, concatMap } from 'rxjs/operators';
+import { EMPTY, of } from 'rxjs';
+
+import * as CounterActions from './counter.actions';
+
+
+
+@Injectable()
+export class CounterEffects {
+
+  loadCounters$ = createEffect(() => this.actions$.pipe(
+    ofType(CounterActions.loadCounters),
+    concatMap(() =>
+      /** An EMPTY observable only emits completion. Replace with your own observable API request */
+      EMPTY.pipe(
+        map(data => CounterActions.loadCountersSuccess({ data })),
+        catchError(error => of(CounterActions.loadCountersFailure({ error }))))
+    )
+  ));
+
+
+
+  constructor(private actions$: Actions) {}
+
+}
